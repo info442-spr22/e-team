6ef6a555b8ae0b5126d2866f27c709e43bc7a1a1
MARY4177 <55933930+MARY4177@users.noreply.github.com>
2022-05-07 01:09:17 -0700

    return to map button

---
+++ e-safety/e-safety-app/src/components/ProfilePage.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import React, { useEffect } from 'react';
+// import firebase from 'firebase/compat/app';
+import { getAuth, signOut } from "firebase/auth";
+import HomePage from './HomePage';
+
+export default function ProfilePage({token}) {
+
+    console.log()
+
+    return (
+    <div>
+        <h1>User's Profile</h1>
+
+        <div class="name">
+            <h2>Name: </h2>
+        </div>
+        <div class="address">
+            <h2>Address: </h2>
+        </div>
+        <div class="bio">
+            <h2>Bio: </h2>
+        </div>
+            <button onClick={<editPage />}>Edit</button>
+        <div class="note">
+            <h3> Make a incident report on the map to secure night walk safer for UW students! </h3>
+        </div>
+        <div class="return" style={{display:'flex', justifyContent:'flex-end'}}>
+            <button type="button" class="btn home" onClick={<HomePage />}>
+                <p> Return to Map Page</p>
+            </button>
+        </div>
+    </div>
+    );
+}
