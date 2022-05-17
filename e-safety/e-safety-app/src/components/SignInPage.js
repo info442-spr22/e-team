79ca5353004991ff8bee8c96964bb39f9a7d2c8f
MARY4177 <55933930+MARY4177@users.noreply.github.com>
2022-05-16 21:49:44 -0700

    styling in signinpage

--- e-safety/e-safety-app/src/components/SignInPage.js
+++ e-safety/e-safety-app/src/components/SignInPage.js
@@ -3,9 +3,12 @@ import React from 'react';
 export default function SignInPage(props) {
   return ( 
     <div>
-      <h1>E-Safety</h1>
-      <h2>Sign in to secure your travel or report an incident to make night walking safer for the Dawg Pack!</h2>
+      <h1>Welcome to E-Safety</h1>
+      <h3>Please sign in to secure your travel or report an incident to make night walking safer for the Dawg Pack!</h3>
+      <img src="../img/Logo.jpg" alt="Profile Building 2 Error screen" height="400"></img>
+      <div>
       <button onClick={props.loginWithGoogle}>Login With Google</button> 
+      </div>
     </div>
   );
 }
