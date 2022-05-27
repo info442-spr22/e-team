41c8b46ef5ca7f74bc7659c66324c88084d01178
MARY4177 <55933930+MARY4177@users.noreply.github.com>
2022-05-27 15:43:28 -0700

    clear instruction on report page

--- e-safety/e-safety-app/src/components/ReportPage.js
+++ e-safety/e-safety-app/src/components/ReportPage.js
@@ -5,12 +5,13 @@ import { Link } from 'react-router-dom';
 export default function ReportPage({token}) {
 
   return (
-    <div class = "report">
+    <div class = "report" >
       <h1>Report an Incident</h1>
+      <h5>Please type in the address and click "confirm" button.
+      </h5>
       <div id = "map">
         <FORM_MODEL />
       </div>
-    </div>
-
+      </div>
   );
 }
