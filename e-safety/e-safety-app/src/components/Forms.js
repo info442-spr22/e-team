5e75d40345c6d97582c0523bf3582bfed7258af6
Stephanie Zhang <stephaniezhang@Stephanies-MacBook-Air-4.local>
2022-05-29 22:04:08 -0700

    added required on forms

--- e-safety/e-safety-app/src/components/Forms.js
+++ e-safety/e-safety-app/src/components/Forms.js
@@ -233,6 +233,7 @@ class FORM_MODEL extends Component {
           placeholderText={'dd/mm/yyyy'}
           showYearDropdown // year show and scrolldown alos
           scrollableYearDropdown
+          required
         />
         <span className="text-danger">{this.state.dateError}</span>
         </div>
@@ -254,7 +255,7 @@ class FORM_MODEL extends Component {
         <label>
           Incident Description:
         </label>
-          <textarea value={this.state.descriptionText} onChange={this.handleDescription}/>
+          <textarea required value={this.state.descriptionText} onChange={this.handleDescription}/>
           <br/>
           <span className="text-danger">{this.state.textError}</span>
         <br />
